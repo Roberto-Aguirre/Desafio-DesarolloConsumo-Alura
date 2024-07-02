80312c83ca030cc490615634a8cfdb02960ee419
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-07-02 17:19:58 -0600

    Initial push

---
+++ src/test/java/com/rias/use/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rias.use.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
